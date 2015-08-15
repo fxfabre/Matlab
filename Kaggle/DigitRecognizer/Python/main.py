f1f2bd44bee6c07e76ea52bdbcd07fe6e1d27122
fxfabre <fxfabre@gmail.com>
2015-08-15 22:31:08 +0200

    Strange results with GridSearchCV

--- Kaggle/DigitRecognizer/Python/main.py
+++ Kaggle/DigitRecognizer/Python/main.py
@@ -9,6 +9,7 @@ import sys
 from datetime import datetime
 
 from sklearn.decomposition import PCA
+from sklearn.metrics import accuracy_score
 
 from sklearn.lda import LDA
 from sklearn.linear_model import LogisticRegression
@@ -19,6 +20,7 @@ from sklearn.neural_network import BernoulliRBM
 
 from sklearn.pipeline import Pipeline
 from sklearn.grid_search import GridSearchCV
+from sklearn.cross_validation import cross_val_score
 
 import xgboost as xgb
 
@@ -26,10 +28,11 @@ import xgboost as xgb
 TRAIN_FILE = "../data/train.csv"
 TEST_FILE  = "../data/test.csv"
 NB_SAMPLES = 5000 # 42000
-NB_THREADS = 2
+NB_THREADS = 1
 VERBOSE_LEVEL = 3
+NB_CV = 4
 
-PCA_VARIANCES = [0.05, 0.90] # , 0.80, 0.85, 0.90, 0.93, 0.96, 0.99]
+PCA_VARIANCES = [0.90] # , 0.80, 0.85, 0.90, 0.93, 0.96, 0.99]
 REGLOG_C = [0.5, 1, 10] # [0.1, 0.25, 0.5, 0.75, 1.0, 3, 10, 100]
 SVM_C    = [0.5, 1, 10] # [0.1, 0.25, 0.5, 0.75, 1.0, 3, 10, 100]
 
@@ -70,18 +73,86 @@ def readTestSet():
 # Tools
 ##########################
 def findBestParams(X, y, fileName, classifier, parameters):
-    print( str( datetime.now() ) + " : Processing of " + filename)
-    outFile = open('Results/ ' + fileName, 'w')
-    sys.stdout = outFile
+    print( str( datetime.now() ) + " : Processing of " + fileName)
 
-    estimator = GridSearchCV(classifier, parameters, cv=2, n_jobs=NB_THREADS, verbose=VERBOSE_LEVEL)
-    estimator.fit(X, y)
+    # Run cross validation and save displayed text in log file
+    with open('Results/ ' + fileName + '.log', 'w', 1) as outFile:     # 0 for unbuffered file
+        sys.stdout = outFile
+        estimator = GridSearchCV(classifier, parameters,
+            cv=NB_CV, n_jobs=NB_THREADS, verbose=VERBOSE_LEVEL, scoring=classifyComputeError)
+        estimator.fit(X, y)
 
-    print( fileName + str(estimator.best_params_) )
-    print( estimator.grid_scores_ )
+    # Set default output for print to the screen (hope there is no print between those 2 lines)
     sys.stdout = sys.__stdout__
-    outFile.close()
-    return estimator.grid_scores_
+
+    # Save best params and each CV score to a file
+    with open('Results/ ' + fileName + '.bestParams', 'w') as outFile:
+        outFile.write( str(estimator.best_params_) )
+    with open('Results/ ' + fileName + '.gridScores', 'w') as outFile:
+        outFile.write( str(estimator.grid_scores_) )
+
+    print( estimator.best_params_ )
+    return
+
+def optimizeParameters(classifier, X, y, n_iter, params={}, steps={}):
+    for n in range( n_iter ):
+        for key, value in params.items():
+            paramsMoins = dict( params ) # clone params
+            paramsMoins[key] = value - steps[key]
+
+
+#############################
+def classifyComputeError(estimator, X, y):
+    print( estimator)
+    print( X )
+    print( y )
+
+    y_hat = estimator.predict( X )
+    return computeError( y, y_hat )
+
+def computeError(y, y_hat):
+    assert(len(y) == len(y_hat))
+    diff = np.array(y) - np.array(y_hat)
+    tmp = [ int(x==0) for x in diff ]
+    return sum(tmp) / len(y)
+
+def classifyData(classifier, X_train, y_train, classifierName=''):
+    """
+    :param classifier: classifier.fit and classifier.predict must be defined
+    :param X_train:         training set
+    :param y_train:         training target / label
+    :param classifierName:  write test estimates to file classifierName.csv
+    :return: predictions for X_test + % of good classification on train set
+    """
+    error_test = 0
+
+    classifier.fit(X_train, y_train)
+    y_hat_train = classifier.predict(X_train)
+    errorTrain = computeError(y_train, y_hat_train)
+    return errorTrain
+
+def knnClassif(X_train, y_train):
+    parameters = {
+        'n_neighbors'  : 1,
+        'weights'      : 'uniform',
+        'algorithm'    : 'kd_tree',
+        'leaf_size'    : 100,
+        'metric'       : 'minkowski',
+        'p'            : 1
+    }
+    knn = KNeighborsClassifier(**parameters)
+    errorTrain = classifyData(knn, X_train, y_train, "KNN")
+    print( 'error KNN : ' + str(errorTrain) )
+    return
+
+def svmClassif(X_train, y_train):
+    pca = PCA(n_components=10)
+    Xt = pca.fit_transform(X_train)
+
+    svm = SVC(kernel='linear')
+    errorTrain = classifyData(svm, Xt, y_train, "SVM")
+    print( 'error SVM : ' + str(errorTrain) )
+    return
 
 
 ##########################
@@ -98,7 +169,7 @@ def findBestParams_LDA(X, y):
         'n_components'  : PCA_VARIANCES,
         'shrinkage'     : [1] # list(np.arange(0.125, 1.1, 0.125)) + [None, 'auto']
     }]
-    findBestParams(X, y, 'Result_LDA', LDA(), parameters)
+    findBestParams(X, y, 'LDA', LDA(), parameters)
     return
 
 def findBestParams_RegLog(X, y):
@@ -190,6 +261,33 @@ def findBestParams_RBM(X, y):
 
 
 ##########################
+# X-Validation, common models V2
+##########################
+def findBestParams_KNN_2(X, y):
+    knn = KNeighborsClassifier()
+    parameters = {
+        'n_neighbors'  : [3], # list(range(1,3,2)),
+        'weights'      : ['uniform', 'distance'],
+        'algorithm'    : ['kd_tree'], # ['ball_tree', 'kd_tree'],
+        'leaf_size'    : [100], # [15, 30, 50, 100],
+        'metric'       : ['minkowski'],
+        'p'            : [1, 2]
+    }
+
+    for variance in PCA_VARIANCES:
+        pca = PCA(variance)
+        X_afterPca = pca.fit_transform( X )
+
+        nComponents = str(pca.n_components_)
+        eigenValues = pca.explained_variance_ratio_
+        explainedVariance = sum(eigenValues)
+        print( "%d components => %d variance".format(nComponents, explainedVariance) )
+
+        findBestParams(X_afterPca, y, 'Result_KNN', knn, parameters)
+
+    return
+
+##########################
 # XGBoost
 ##########################
 def XgradientBoost(X, y, X_test):
@@ -231,21 +329,19 @@ def main():
     assert(n_raw == n_test)
 
     # ==== Cross validation & estimation ====
-    findBestParams_LDA(X_raw, y)
-    findBestParams_RegLog(X_raw, y)
-    findBestParams_KNN(X_raw, y)
-    findBestParams_RandomForest(X_raw, y)
-    findBestParams_SVM(X_raw, y)
-    findBestParams_RBM(X_raw, y)
+#    findBestParams_LDA(X_raw, y)
+#    findBestParams_RegLog(X_raw, y)
+#    findBestParams_KNN_2(X_raw, y)
+#    findBestParams_RandomForest(X_raw, y)
+#    findBestParams_SVM(X_raw, y)
+#    findBestParams_RBM(X_raw, y)
+
+    knnClassif(X_raw, y)
+    svmClassif(X_raw, y)
 
 #    XgradientBoost(X_raw, y, X_test)
 
     return
-
-
-
-
-
 
 
 #######################
