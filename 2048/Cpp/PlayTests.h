69a25375d77a573a8ba5af76af73c128d7ea34e6
fxfabre <fx.fabre@gmail.com>
2016-11-30 00:14:58 +0000

    remove old projects

--- 2048/Cpp/PlayTests.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "ExpectiMax.h"
-#include "BoardDB.h"
-
-unsigned int PlayTest(const HeuristicParameters& parameters, unsigned int rollbacks, BoardDB* boarddb = NULL);
-
-void PlayTest_Tune(unsigned int rollbacks, unsigned int plays, unsigned int population_size, unsigned int tournament_size, unsigned int latency);
-void PlayTest_Batch(unsigned int rollbacks, unsigned int plays, BoardDB* boarddb);
